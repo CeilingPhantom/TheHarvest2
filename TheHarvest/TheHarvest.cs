679f84a7dbbc0fecff1a044ba6d122a22ce7125b
James Luo <jameshcluo@gmail.com>
2020-12-15 02:37:37 +1100

    init

---
+++ TheHarvest/TheHarvest.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Xna.Framework;
+using Nez;
+
+namespace TheHarvest
+{
+    public class TheHarvest : Core
+    {
+        protected override void Initialize()
+        {
+            base.Initialize();
+            Window.AllowUserResizing = true;
+        }
+    }
+}
